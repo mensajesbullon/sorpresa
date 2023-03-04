48ddf0e22f3f41c4a7e2e5b709e47fda23a4743d
Edu <84049228+Edu@users.noreply.github.com>
2023-03-04 01:11:08 -0500

    V1
    S

---
+++ js/main.js
@@ -0,0 +1,39 @@
+
+// PRIMER MENSAJE
+
+setTimeout(function () {
+    const message = document.getElementById('mensaje');
+    message.innerHTML = "Hola Amor!"
+}, 1000);
+
+setTimeout(function () {
+    const message = document.getElementById('mensaje');
+    message.innerHTML = "Espero que"
+}, 2500);
+
+setTimeout(function () {
+    const message = document.getElementById('mensaje');
+    message.innerHTML = "estés bien."
+}, 4000);
+
+setTimeout(function () {
+    const message = document.getElementById('mensaje');
+    message.innerHTML = "Esta pequeña sorpresa"
+}, 5500);
+
+setTimeout(function () {
+    const message = document.getElementById('mensaje');
+    message.innerHTML = "es para ti."
+}, 7000);
+
+setTimeout(function () {
+    const message = document.getElementById('mensaje');
+    message.innerHTML = "TE AMO <3 Sindy"
+}, 8500);
+
+// AUTOR
+
+setTimeout(function () {
+    const message = document.getElementById('autor');
+    message.innerHTML = "de Edu"
+}, 4300);
